a1bf1116c9eb48204376e08baaf601ff81f8b2f7
Biplab Neupane <biplabneupane43@gmail.com>
2022-04-18 09:32:22 +0545

    Rooms added

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="">
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
+            <div class="carousel-inner" role="listbox" style="max-width:1920px; max-height:650px !important;">
                 <div class="carousel-item active">
                     <img class="d-block w-100" src="images/b2.png" alt="First slide">
                 </div>
@@ -11,7 +11,7 @@
                     <img class="d-block w-100" src="images/b1.png" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="images/b2.png" alt="Third slide">
+                    <img class="d-block w-100 h-50" src="images/b2.png" alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -23,7 +23,7 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
-        <br><br>
+        
         <hr>
     </div>
     <div class="container">
